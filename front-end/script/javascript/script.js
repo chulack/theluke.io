6ea2a674b35a6a9cb2f566c93e7b5e3fba485eea
Luke Chulack ( Čuljak ) <70914959+chulack@users.noreply.github.com>
2022-05-03 08:15:23 -0600

    edits for project 1

--- front-end/script/javascript/script.js
+++ front-end/script/javascript/script.js
@@ -4,21 +4,30 @@
 ***
 *Original Author: Luke Chulack                         
 *Date Created: 02/26/2021                                     
-*Version: 1.0                                           
-*Date Last Modified: 02/26/2021                               
+*Version: 3.0                                           
+*Date Last Modified: 05/03/2022                              
 *Modified by: Luke Chulack        
 *Modification log:  
     version 1.0 - 02/26/2021  - added the needed code for fullpage and built the hamburger menu following the outline hamburger menu of w3school. setup basic verification for form. 
     version 2.0 - 05/05/2021  - added code to allow menu on all pages, made menu ui better by making all page elements go to none while menu is open.
+    version 3.0 - 05/03/2022  - removed code for contact form that was just repeating, added link redirect using javascript, add logic for loader on start up, and added fixed menu so when item is click the menu leaves.
 
     ***
 ******************************************************************** */
 
 
 const $ = (e) => document.querySelector(e);
+const redirect = (e) => location.href = e;
 
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    
+ const main = () => {
+
+
+   $("#all").classList.toggle("hide");
+   $("#spinner").classList.toggle("hide");
 
     console.log("I am useing fullPgae.JS under the GPL 3 Licence, the full source code will be under GPL 3 here: https://github.com/chulack/theluke.io")
 
@@ -103,8 +112,60 @@ $(".closebtn").addEventListener("click", ()=>{
 });
 
 
+// close menu when page is clicked
+$("#homeBTN").addEventListener("click", ()=>{
 
+    $("#myNav").style.width = "0%";
+    $("#all_class_1").classList.toggle("remove");
+    $("#all_class_2").classList.toggle("remove");
+    $("#all_class_3").classList.toggle("remove");
+
+    $("#fp-nav").classList.toggle("remove");
+});
   
+
+$("#aboutBTN").addEventListener("click", ()=>{
+
+    $("#myNav").style.width = "0%";
+    $("#all_class_1").classList.toggle("remove");
+    $("#all_class_2").classList.toggle("remove");
+    $("#all_class_3").classList.toggle("remove");
+
+    $("#fp-nav").classList.toggle("remove");
+});
+  
+
+
+
+// links for github
+
+
+$("#snelfu_webpage").addEventListener("click",  () =>{
+    redirect("https://chulack.github.io/snelfu_webpage/index.html");
+
+});
+$("#boise_noodle_final").addEventListener("click",  () =>{
+    redirect("https://chulack.github.io/boise_noodle_final/index.html");
+
+});
+$("#new_offer").addEventListener("click",  () =>{
+    redirect("https://chulack.github.io/new_offer/index.html");
+
+});
+
+$("#boiserock").addEventListener("click",  () =>{
+    redirect("http://boiserock.com");
+
+});
+$("#pop_magic_final").addEventListener("click",  () =>{
+    redirect("https://chulack.github.io/pop_magic_final/index.html");
+
+});
+$("#space_invaders").addEventListener("click",  () =>{
+    redirect("https://chulack.github.io/space_invaders/index.html");
+
+});
+
 
     // hamburger menu for about 
 
@@ -145,38 +206,9 @@ $(".closebtn").addEventListener("click", ()=>{
     });
 
 
+ }
 
-    // form verification
-    $("#send_form").addEventListener("click", ()=>{
-
-        const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
-
-        let error = 0;
-
-
-
-        if($("#name").value.trim() ==  "") {
-            error += 1;
-        }
-
-    
-    if($("#email").value.trim() ==  ""  || !emailPattern.test(email.value.trim()) )
-    {
-        error += 1;
-    }
-
-    if($("#msg").value.trim() ==  "")
-    {
-        error += 1;
-    }
-
-    if(error == 0) {
-        alert("Form submited")
-        $("#form_main").submit();
-    }
-
-});
-
+ setTimeout(main, 1000);
 
 
 });
